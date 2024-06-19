1eaa64f350221bb72b4d2adb0eaa511e529cab82
k2ntum <khainguyen@umass.edu>
2024-06-19 13:25:15 -0400

    [add] change port

--- projects/sinfonia/scripts/transform_carbon_traces.py
+++ projects/sinfonia/scripts/transform_carbon_traces.py
@@ -6,21 +6,17 @@ from datetime import datetime
 CARBON_TRACES_FOLDER = 'carbon_traces'
 
 
-# Get the list of CSV files in the carbon_traces folder
 csv_files = [file for file in os.listdir(CARBON_TRACES_FOLDER) if file.endswith('.csv')]
 
-# Iterate over each CSV file
+
 for file in csv_files:
     print(file)
     
-    # Read the CSV file
     df = pd.read_csv(os.path.join(CARBON_TRACES_FOLDER, file))
     
-    # Convert the 'Datetime (UTC)' column to Unix timestamp
     if 'Datetime (UTC)' in df.columns:
         df['Datetime (UTC)'] = df['Datetime (UTC)'].apply(lambda x: int(datetime.strptime(x, '%Y-%m-%d %H:%M:%S').timestamp()))
     
-    # Rename the 'Datetime (UTC)' column to 'timestamp'
     df = df.rename(
         columns={
             'Datetime (UTC)': 'timestamp',
@@ -33,6 +29,4 @@ for file in csv_files:
             }
         )
     
-    # Rewrite the file with the updated data
     df.to_csv(os.path.join(CARBON_TRACES_FOLDER, file), index=False)
-
